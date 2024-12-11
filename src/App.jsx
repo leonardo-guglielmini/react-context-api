05389caa08577cb0b5e7f118c4bebbcd1f41e164
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-12-11 14:27:55 +0100

    ADD GlobalContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 
 /*IMPORT UTILITIES*/
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import GlobalContext from './context/GlobalContext'
 
 /*IMPORT BASE LAYOUT*/
 import Blank from './layouts/Blank'
@@ -15,28 +18,46 @@ import Posts from './pages/Posts/Posts'
 import NotFound from './pages/NotFound/NotFound'
 import Post from "./pages/Posts/Post/Post"
 
-
+export const API_BASE_URI = 'http://localhost:3000/'
 
 function App() {
 
+  const [posts, setPosts] = useState([])
+
+  function fetchPosts(){
+    axios.get(`${API_BASE_URI}posts`)
+        .then(res => {
+            console.log("fetch posts")
+            console.log(res.data)
+            setPosts(res.data)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+}
+
+useEffect(() => {
+    fetchPosts()
+},[])
+
   return (
-    <>
-    <BrowserRouter>
-      <Routes >
-        <Route element={<Default/>}>
-          <Route path='/' element={<Homepage/>}></Route>
-          <Route path='/posts'>
-            <Route index Component={Posts}></Route>
-            <Route path=":id" Component={Post}></Route>
+    <GlobalContext.Provider value={{posts, fetchPosts}}>
+      <BrowserRouter>
+        <Routes >
+          <Route element={<Default/>}>
+            <Route path='/' element={<Homepage/>}></Route>
+            <Route path='/posts'>
+              <Route index Component={Posts}></Route>
+              <Route path=":id" Component={Post}></Route>
+            </Route>
+            <Route path='/about-us' element={<About/>}></Route>
           </Route>
-          <Route path='/about-us' element={<About/>}></Route>
-        </Route>
-        <Route element={<Blank/>}>
-          <Route path='*' element={<NotFound/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </>
+          <Route element={<Blank/>}>
+            <Route path='*' element={<NotFound/>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GlobalContext.Provider>
   )
 }
 
